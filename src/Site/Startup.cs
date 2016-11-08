e9d7c39ff6ae8fae61e20f61be8eefcefa602acc
Andre Calil <andre@calil.com.br>
2016-11-08 03:06:53 -0200

    Startup 3

--- src/Site/Startup.cs
+++ src/Site/Startup.cs
@@ -12,8 +12,8 @@ namespace Site
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("appsettings.dev.json", optional: true)
+                //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                //.AddJsonFile("appsettings.dev.json", optional: true)
                 .AddEnvironmentVariables();
 
             //if (env.IsDevelopment())
